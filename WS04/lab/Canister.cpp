5549a0e0529a1d9746778c2890060f693346ef90
Prarthan Christian <pchristian3@myseneca.ca>
2023-06-08 18:32:16 -0400

    WS04

--- WS04/lab/Canister.cpp
+++ WS04/lab/Canister.cpp
@@ -1,4 +1,4 @@
- #define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <istream>
 #include <iomanip>
 #include "cstring.h"  // using strLen, strCpy and strCmp 
@@ -36,7 +36,7 @@ namespace sdds {
 		int result;
 		result = strCmp(C.m_contentName, m_contentName);
 
-		if (result == 0) 
+		if (result == 0)
 		{
 			ok = true;
 			return ok;
@@ -45,9 +45,9 @@ namespace sdds {
 		{
 			ok = false;
 			return ok;
-			
+
 		}
-	}	
+	}
 
 	void Canister::setName(const char* Cstr) {
 		if (Cstr != nullptr && m_usable) {
@@ -62,7 +62,7 @@ namespace sdds {
 			//do nothing
 		}
 	}
-	
+
 	Canister::Canister() {
 		setToDefault();
 	}
@@ -70,13 +70,122 @@ namespace sdds {
 	Canister::Canister(const char* contentName) {
 		setToDefault();
 		setName(contentName);
-		
+
 	}
 
 	Canister::Canister(double hieght, double diameter, const char* contentName) {
 		setToDefault();
-		
+		if (m_height > 0.0 && m_diameter > 0.0) {
+			m_height = hieght;
+			m_diameter = diameter;
+			setName(contentName);
+		}
+		else
+		{
+			m_usable = false;
+		}
+
 	}
 
+	Canister::~Canister() {
+		delete[] m_contentName;
+	}
+
+	void Canister::clear() {
+		delete[] m_contentName;
+		m_contentName = nullptr;
+		m_contentVolume = 0.0;
+		m_usable = true;
+
+	}
+
+	double Canister::capacity() const {
+
+		double radius = m_diameter / 2.0;
+		double capacity = PI * radius * radius * m_height;
+		return capacity;
+	}
+
+	double Canister::volume() const {
+		return m_contentVolume;
+	}
+
+	Canister& Canister::setContent(const char* contentName) {
+		if (contentName == nullptr) 
+		{
+			m_usable = false;
+		}
+		else if(isEmpty())
+		{
+			setName(contentName);
+		}
+		else if (!hasSameContent(contentName))
+		{
+			m_usable = false;
+		}
+		return *this;
+	}
+
+	Canister& Canister::pour(double quantity) {
+		if (m_usable && quantity > 0)
+		{
+
+			if ((quantity + volume()) <= capacity())
+			{
+				m_contentVolume += quantity;
+			}
+			else
+			{
+				m_usable = false;
+			}
+		
+		}
+		else
+		{
+			m_usable = false;
+		}
+
+		return *this;
+	}
+
+	Canister& Canister::pour(Canister& C) {
+		setContent(C.m_contentName);
+
+		if (C.m_contentVolume > this->capacity() - this->volume())
+		{
+			C.m_contentVolume = C.m_contentVolume - (this->capacity() - this->volume());
+			this->m_contentVolume = this->capacity();
+		}
+		else
+		{
+			pour(C.volume());
+			C.m_contentVolume = 0.0;
+		}
+
+		return *this;
+	}
+
+	std::ostream& Canister::display()const
+	{
+		cout.setf(ios::fixed);
+		cout.precision(1);
+		cout.width(7);
+		cout << capacity() << "cc (" << m_height << "x" << m_diameter << ") Canister";
+		if (!m_usable)
+		{
+			cout << " of Unusable content, discard!";
+		}
+		else
+		{
+			if (m_contentName)
+			{
+				cout << " of ";
+				cout.width(7);
+				cout << volume() << "cc   " << m_contentName;
+			}
+
+		}
+		return cout;
+	}
 
 }
